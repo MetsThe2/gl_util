d4e1ee98c891d880312bcb7d42b32886c9d819a8
Mets <example@example.com>
2014-08-27 23:04:24 +0300

    New dependencies
    math_def.h contains math.h + frequently used constants.
    pp_sak.h (Preprocessor Swiss Army Knife) is what it sounds like (except
    for being almost empty).

---
+++ math_def.h
@@ -0,0 +1,10 @@
+#ifndef MATH_DEF_H_INCLUDED
+#define MATH_DEF_H_INCLUDED
+
+#include <math.h>
+#ifndef M_PI
+#define M_PI     3.14159265358979323846f
+#endif
+#define M_SQRT_2 1.41421356237309504880f
+
+#endif // MATH_DEF_H_INCLUDED
---
+++ pp_sak.h
@@ -0,0 +1,6 @@
+#ifndef PP_SAK_H_INCLUDED
+#define PP_SAK_H_INCLUDED
+
+#define ARRAY_LEN(a) (sizeof(a) / sizeof(a[0]))
+
+#endif // PP_SAK_H_INCLUDED
