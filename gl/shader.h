821ae9f64c7daf0c7e70aad127b920fd2a008258
MetsThe2 <MetsThe2@users.noreply.github.com>
2014-10-26 16:01:14 +0200

    .

--- gl/shader.h
+++ gl/shader.h
@@ -14,13 +14,14 @@ GLuint get_shader(GLenum type, char *filename);
 #define GET_SHADER_c(s) get_shader(GL_COMPUTE_SHADER,  s ".comp")
 #define GET_SHADER2(f, s) f(s)
 #define GET_SHADER(n, t) GET_SHADER2(GET_SHADER_ ## t, #n)
-/* If s isn't one of v (vertex), f (fragment), g (geometry) or c (compute),
-concatenating GET_SHADER and t will yield a macro that doesn't exist and
+/* If `t` isn't one of v (vertex), f (fragment), g (geometry) or c (compute),
+concatenating `GET_SHADER_` and `t` will yield a macro that doesn't exist and
 result in a compiler error. */
 
 GLuint make_prog(GLuint vert, GLuint frag, void (*before_linking)(GLuint prog));
 GLuint get_prog(char *vert_filename, char *frag_filename,
                 void (*before_linking)(GLuint prog));
+// TODO: stringify `v` and `f` in `GET_PROG`.
 #define GET_PROG(v, f, b) get_prog(v ".vert", f ".frag", b)
 
 void del_progs(GLuint *progs, unsigned prog_num);
